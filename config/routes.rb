480217a510d550e8e04fde0325cb746252784f95
Nico Farr <nicofarr@gmail.com>
2022-06-05 21:46:20 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/login", :controller => "sessions", :action => "new"
   get "/logout", :controller => "sessions", :action => "destroy"
   get "/delete", :controller => "posts", :action => "destroy"
-  get "/edit", :controller => "posts", :action => "update"
+  get "/edit", :controller => "posts", :action => "edit"
 
   resources "posts"
   resources "sessions"
